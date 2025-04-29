0ecca39396d93f2019f78e77ad9fdc6aab5ccc3f
Bruno <bruno.morais@vsgroup.com.br>
2025-04-29 20:07:00 -0300

    feat/ create logger

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/brunoh-android/api_vagas_emprego.git/router"
+import (
+	"github.com/brunoh-android/api_vagas_emprego.git/config"
+	"github.com/brunoh-android/api_vagas_emprego.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("error on init: %v", err)
+		panic(err)
+	}
 	router.Initialize()
 }
