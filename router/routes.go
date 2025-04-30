ed8e002846051806d1a4870a39ec5810c544c95f
Bruno <bruno.morais@vsgroup.com.br>
2025-04-30 15:37:11 -0300

    feat/ add swagger

--- router/routes.go
+++ router/routes.go
@@ -1,13 +1,18 @@
 package router
 
 import (
+	"github.com/brunoh-android/api_vagas_emprego.git/docs"
 	"github.com/brunoh-android/api_vagas_emprego.git/handler"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
-	
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
@@ -16,4 +21,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningHandler)
 	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
